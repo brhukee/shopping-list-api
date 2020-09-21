0cd5bfaae5ac51727b66bb65d753fcf7fb438644
Brandon Hukee <brandonhukee@Brandons-MacBook-Air.local>
2020-09-21 07:41:58 -0600

    Finished API

--- test/example.js
+++ test/example.js
@@ -1,6 +1,6 @@
 process.env.TESTENV = true
 
-let Example = require('../app/models/example.js')
+let List = require('../app/models/list.js')
 let User = require('../app/models/user')
 
 const crypto = require('crypto')
@@ -14,16 +14,16 @@ chai.use(chaiHttp)
 
 const token = crypto.randomBytes(16).toString('hex')
 let userId
-let exampleId
+let listId
 
-describe('Examples', () => {
-  const exampleParams = {
+describe('Lists', () => {
+  const listParams = {
     title: '13 JavaScript tricks SEI instructors don\'t want you to know',
     text: 'You won\'believe number 8!'
   }
 
   before(done => {
-    Example.deleteMany({})
+    List.deleteMany({})
       .then(() => User.create({
         email: 'caleb',
         hashedPassword: '12345',
@@ -33,49 +33,49 @@ describe('Examples', () => {
         userId = user._id
         return user
       })
-      .then(() => Example.create(Object.assign(exampleParams, {owner: userId})))
+      .then(() => List.create(Object.assign(listParams, {owner: userId})))
       .then(record => {
-        exampleId = record._id
+        listId = record._id
         done()
       })
       .catch(console.error)
   })
 
-  describe('GET /examples', () => {
-    it('should get all the examples', done => {
+  describe('GET /lists', () => {
+    it('should get all the lists', done => {
       chai.request(server)
-        .get('/examples')
+        .get('/lists')
         .set('Authorization', `Token token=${token}`)
         .end((e, res) => {
           res.should.have.status(200)
-          res.body.examples.should.be.a('array')
-          res.body.examples.length.should.be.eql(1)
+          res.body.lists.should.be.a('array')
+          res.body.lists.length.should.be.eql(1)
           done()
         })
     })
   })
 
-  describe('GET /examples/:id', () => {
-    it('should get one example', done => {
+  describe('GET /lists/:id', () => {
+    it('should get one list', done => {
       chai.request(server)
-        .get('/examples/' + exampleId)
+        .get('/lists/' + listId)
         .set('Authorization', `Token token=${token}`)
         .end((e, res) => {
           res.should.have.status(200)
-          res.body.example.should.be.a('object')
-          res.body.example.title.should.eql(exampleParams.title)
+          res.body.list.should.be.a('object')
+          res.body.list.title.should.eql(listParams.title)
           done()
         })
     })
   })
 
-  describe('DELETE /examples/:id', () => {
-    let exampleId
+  describe('DELETE /lists/:id', () => {
+    let listId
 
     before(done => {
-      Example.create(Object.assign(exampleParams, { owner: userId }))
+      List.create(Object.assign(listParams, { owner: userId }))
         .then(record => {
-          exampleId = record._id
+          listId = record._id
           done()
         })
         .catch(console.error)
@@ -83,7 +83,7 @@ describe('Examples', () => {
 
     it('must be owned by the user', done => {
       chai.request(server)
-        .delete('/examples/' + exampleId)
+        .delete('/lists/' + listId)
         .set('Authorization', `Bearer notarealtoken`)
         .end((e, res) => {
           res.should.have.status(401)
@@ -93,7 +93,7 @@ describe('Examples', () => {
 
     it('should be succesful if you own the resource', done => {
       chai.request(server)
-        .delete('/examples/' + exampleId)
+        .delete('/lists/' + listId)
         .set('Authorization', `Bearer ${token}`)
         .end((e, res) => {
           res.should.have.status(204)
@@ -103,7 +103,7 @@ describe('Examples', () => {
 
     it('should return 404 if the resource doesn\'t exist', done => {
       chai.request(server)
-        .delete('/examples/' + exampleId)
+        .delete('/lists/' + listId)
         .set('Authorization', `Bearer ${token}`)
         .end((e, res) => {
           res.should.have.status(404)
@@ -112,16 +112,16 @@ describe('Examples', () => {
     })
   })
 
-  describe('POST /examples', () => {
-    it('should not POST an example without a title', done => {
+  describe('POST /lists', () => {
+    it('should not POST an list without a title', done => {
       let noTitle = {
         text: 'Untitled',
         owner: 'fakedID'
       }
       chai.request(server)
-        .post('/examples')
+        .post('/lists')
         .set('Authorization', `Bearer ${token}`)
-        .send({ example: noTitle })
+        .send({ list: noTitle })
         .end((e, res) => {
           res.should.have.status(422)
           res.should.be.a('object')
@@ -129,15 +129,15 @@ describe('Examples', () => {
         })
     })
 
-    it('should not POST an example without text', done => {
+    it('should not POST an list without text', done => {
       let noText = {
-        title: 'Not a very good example, is it?',
+        title: 'Not a very good list, is it?',
         owner: 'fakeID'
       }
       chai.request(server)
-        .post('/examples')
+        .post('/lists')
         .set('Authorization', `Bearer ${token}`)
-        .send({ example: noText })
+        .send({ list: noText })
         .end((e, res) => {
           res.should.have.status(422)
           res.should.be.a('object')
@@ -147,36 +147,36 @@ describe('Examples', () => {
 
     it('should not allow a POST from an unauthenticated user', done => {
       chai.request(server)
-        .post('/examples')
-        .send({ example: exampleParams })
+        .post('/lists')
+        .send({ list: listParams })
         .end((e, res) => {
           res.should.have.status(401)
           done()
         })
     })
 
-    it('should POST an example with the correct params', done => {
-      let validExample = {
+    it('should POST an list with the correct params', done => {
+      let validList = {
         title: 'I ran a shell command. You won\'t believe what happened next!',
         text: 'it was rm -rf / --no-preserve-root'
       }
       chai.request(server)
-        .post('/examples')
+        .post('/lists')
         .set('Authorization', `Bearer ${token}`)
-        .send({ example: validExample })
+        .send({ list: validList })
         .end((e, res) => {
           res.should.have.status(201)
           res.body.should.be.a('object')
-          res.body.should.have.property('example')
-          res.body.example.should.have.property('title')
-          res.body.example.title.should.eql(validExample.title)
+          res.body.should.have.property('list')
+          res.body.list.should.have.property('title')
+          res.body.list.title.should.eql(validList.title)
           done()
         })
     })
   })
 
-  describe('PATCH /examples/:id', () => {
-    let exampleId
+  describe('PATCH /lists/:id', () => {
+    let listId
 
     const fields = {
       title: 'Find out which HTTP status code is your spirit animal',
@@ -184,15 +184,15 @@ describe('Examples', () => {
     }
 
     before(async function () {
-      const record = await Example.create(Object.assign(exampleParams, { owner: userId }))
-      exampleId = record._id
+      const record = await List.create(Object.assign(listParams, { owner: userId }))
+      listId = record._id
     })
 
     it('must be owned by the user', done => {
       chai.request(server)
-        .patch('/examples/' + exampleId)
+        .patch('/lists/' + listId)
         .set('Authorization', `Bearer notarealtoken`)
-        .send({ example: fields })
+        .send({ list: fields })
         .end((e, res) => {
           res.should.have.status(401)
           done()
@@ -201,9 +201,9 @@ describe('Examples', () => {
 
     it('should update fields when PATCHed', done => {
       chai.request(server)
-        .patch(`/examples/${exampleId}`)
+        .patch(`/lists/${listId}`)
         .set('Authorization', `Bearer ${token}`)
-        .send({ example: fields })
+        .send({ list: fields })
         .end((e, res) => {
           res.should.have.status(204)
           done()
@@ -212,32 +212,32 @@ describe('Examples', () => {
 
     it('shows the updated resource when fetched with GET', done => {
       chai.request(server)
-        .get(`/examples/${exampleId}`)
+        .get(`/lists/${listId}`)
         .set('Authorization', `Bearer ${token}`)
         .end((e, res) => {
           res.should.have.status(200)
           res.body.should.be.a('object')
-          res.body.example.title.should.eql(fields.title)
-          res.body.example.text.should.eql(fields.text)
+          res.body.list.title.should.eql(fields.title)
+          res.body.list.text.should.eql(fields.text)
           done()
         })
     })
 
     it('doesn\'t overwrite fields with empty strings', done => {
       chai.request(server)
-        .patch(`/examples/${exampleId}`)
+        .patch(`/lists/${listId}`)
         .set('Authorization', `Bearer ${token}`)
-        .send({ example: { text: '' } })
+        .send({ list: { text: '' } })
         .then(() => {
           chai.request(server)
-            .get(`/examples/${exampleId}`)
+            .get(`/lists/${listId}`)
             .set('Authorization', `Bearer ${token}`)
             .end((e, res) => {
-              res.should.have.status(200)
+              res.should.have.statList
               res.body.should.be.a('object')
-              // console.log(res.body.example.text)
-              res.body.example.title.should.eql(fields.title)
-              res.body.example.text.should.eql(fields.text)
+              // console.log(res.body.list.text)
+              res.body.list.title.should.eql(fields.title)
+              res.body.list.text.should.eql(fields.text)
               done()
             })
         })
--- test/user.js
+++ test/user.js
@@ -126,14 +126,14 @@ describe('Users', () => {
         })
     })
 
-    it('the token should allow you to GET /examples', done => {
+    it('the token should allow you to GET /lists', done => {
       chai.request(server)
-        .get('/examples')
+        .get('/lists')
         .set('Authorization', `Token token=${token}`)
         .end((e, res) => {
           res.should.have.status(200)
-          res.body.should.have.property('examples')
-          res.body.examples.should.be.a('array')
+          res.body.should.have.property('lists')
+          res.body.lists.should.be.a('array')
           done()
         })
     })
